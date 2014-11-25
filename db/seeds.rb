14abd7d5d22adb53ca9d3de881bf0a78ff2729f8
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-11-25 17:03:09 +0100

    routes ok - table ok - controller begin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  article = Article.new(
+    title: Faker::Company.name,
+    content: Faker::Company.catch_phrase
+  )
+  article.save
+end
